b4c817a34538edfa56e60947000ffccd5e904ec9
AngelEdOS <porellana@tigo.com.gt>
2023-04-19 14:57:11 +0000

    added pgAdmin

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # zammad-microk8s-deploy
 
-This is a script to deploy zammad using microk8s. You must have to give as argument the port that you want to expose for the application
+This is a script to deploy zammad using microk8s. You must have to give as argument the port that you want to expose for the application, and a second argument to indicate the port for pgAdmin
+
+# Add new ticket state
+
+
+To add new ticket statuses it is necessary to first execute the following:
+
+microk8s kubectl exec -it zammad-0 -n zammad /bin/bash
+
+
+then inside the console of the pod execute:
+
+rails console
+
+
+After that, follow the steps described in https://docs.zammad.org/en/latest/admin/console/working-on-tickets.html#add-new-ticket-state to add a new ticket status.
+
+
+To make the added ticket statuses visible, execute the following inside the console
+of rails
+
+attribute = ObjectManager::Attribute.get(
+     object: 'Ticket',
+     name: 'state_id',
+   )
+
+attribute.data_option[:filter] = Ticket::State.by_category(:viewable).pluck(:id)
+attribute.screens[:create_middle]['ticket.agent'][:filter] = Ticket::State.by_category(:viewable_agent_new).pluck(:id)
+attribute.screens[:create_middle]['ticket.customer'][:filter] = Ticket::State.by_category(:viewable_customer_new).pluck(:id)
+attribute.screens[:edit]['ticket.agent'][:filter] = Ticket::State.by_category(:viewable_agent_edit).pluck(:id)
+attribute.screens[:edit]['ticket.customer'][:filter] = Ticket::State.by_category(:viewable_customer_edit).pluck(:id)
+attribute.save!
--- zammad.sh
+++ zammad.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-if [ $# != 1 ]; then
+if [ $# != 2 ]; then
   echo "Es necesario pasar un argumento especificando el numero
-de puerto con el que se accederá a zammad"
+de puerto con el que se accederá a zammad, y como segundo argumento
+el puerto por donde se accederá al pgAdmin"
   exit 1
 fi
 
 if [ $1 -lt 30000 ] ;then
   echo "El puerto debe ser mayor o igual a 30,000"
   exit 1
+fi
+
+if [ $2 -lt 30000 ] ;then
+  echo "El puerto debe ser mayor o igual a 30,000"
+  exit 2
 fi
 
 if  which "microk8s" > /dev/null ; then
@@ -76,6 +82,53 @@ spec:
 
 microk8s kubectl apply -f svc-zammad-override.yaml
 
+echo "apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: pgadmin
+spec:
+  selector:
+   matchLabels:
+    app: pgadmin
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: pgadmin
+    spec:
+      containers:
+        - name: pgadmin4
+          image: dpage/pgadmin4
+          env:
+            - name: PGADMIN_DEFAULT_EMAIL
+              value: \"admin@admin.com\"
+            - name: PGADMIN_DEFAULT_PASSWORD
+              value: \"admin\"
+            - name: PGADMIN_PORT
+              value: \"80\"
+          ports:
+            - containerPort: 80
+              name: pgadminport
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: pgadmin
+  labels:
+    app: pgadmin
+spec:
+  selector:
+    app: pgadmin
+  type: NodePort
+  ports:
+    - port: 80
+      nodePort: $2" > pgAdmin.yml
+
+microk8s kubectl apply -f pgAdmin.yml -n zammad
+
 rm svc-zammad-override.yaml
+rm pgAdmin.yml
+
 
 echo "/n/n zammad running in port $1"
+echo "/n/n pgAdmin running in port $2"
